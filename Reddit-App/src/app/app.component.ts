0c6a3b7daaa4227efe5c8fdbc08d5519d147b1fe
QuckquackMF <toci.iljas@iisgalvanimi.edu.it>
2025-04-02 12:56:50 +0000

    punto 1

---
+++ Reddit-App/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+  return false;
+  }
+  }
