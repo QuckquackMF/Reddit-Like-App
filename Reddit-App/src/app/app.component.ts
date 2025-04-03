ca17906ffd3252c1b3dcfcc4fe00928e4880cf24
QuckquackMF <toci.iljas@iisgalvanimi.edu.it>
2025-04-03 08:55:08 +0000

    another commit

--- Reddit-App/src/app/app.component.ts
+++ Reddit-App/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from './article/article.component';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,17 @@ import { ArticleComponent } from './article/article.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  articles:Article[];   // <-- component property
+  constructor(){
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-  return false;
+    this.articles.push(new Article('Angular 2', 'http://angular.io', 3));
+    return false;
   }
-  }
+}
